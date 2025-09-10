21bb10d6c3b9dc88c2c49af4d4b24667848ae889
lisantov <lisantov@yandex.ru>
2025-09-10 11:36:56 +0700

    Добавлено открытие-закрытие сайдбара с настройками, их применение к стилям страницы и сброс настроек

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,133 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import {
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import { StyleValue } from 'src/index';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onSubmit: (newStyles: StyleValue) => void;
+	initialState?: StyleValue;
+	isInitialOpen?: boolean;
+};
+
+export const ArticleParamsForm = ({
+	isInitialOpen = false,
+	initialState = {
+		fontFamily: defaultArticleState.fontFamilyOption,
+		fontSize: defaultArticleState.fontSizeOption,
+		fontColor: defaultArticleState.fontColor,
+		containerWidth: defaultArticleState.contentWidth,
+		backgroundColor: defaultArticleState.backgroundColor,
+	},
+	onSubmit,
+}: ArticleParamsFormProps) => {
+	const asideRef = useRef<HTMLElement | null>(null);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [formValue, setFormValue] = useState<StyleValue>(initialState);
+	const handleClick = () => {
+		isOpen ? close() : open();
+	};
+	const open = () => {
+		asideRef.current?.classList.add(styles.container_open);
+		setIsOpen(true);
+	};
+	const close = () => {
+		asideRef.current?.classList.remove(styles.container_open);
+		setIsOpen(false);
+	};
+
+	useEffect(() => {
+		isInitialOpen ? open() : close();
+	}, []);
+
+	const handleChange = (name: string) => (selected: OptionType) => {
+		setFormValue({
+			...formValue,
+			[name]: selected,
+		});
+	};
+
+	const handleSubmit = (event: SyntheticEvent) => {
+		event.preventDefault();
+		onSubmit({
+			fontFamily: formValue.fontFamily,
+			fontSize: formValue.fontSize,
+			fontColor: formValue.fontColor,
+			containerWidth: formValue.containerWidth,
+			backgroundColor: formValue.backgroundColor,
+		});
+	};
+
+	const handleReset = (event: SyntheticEvent) => {
+		event.preventDefault();
+		setFormValue(initialState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleClick} />
+			<aside ref={asideRef} className={styles.container}>
 				<form className={styles.form}>
+					<div className={styles.topContainer}>
+						<Select
+							selected={formValue.fontFamily}
+							options={fontFamilyOptions}
+							title='Шрифт'
+							onChange={handleChange('fontFamily')}
+						/>
+						<RadioGroup
+							name='fontSize'
+							selected={formValue.fontSize}
+							options={fontSizeOptions}
+							title='Размер шрифта'
+							onChange={handleChange('fontSize')}
+						/>
+						<Select
+							selected={formValue.fontColor}
+							options={fontColors}
+							title='Цвет шрифта'
+							onChange={handleChange('fontColor')}
+						/>
+						<Separator />
+						<Select
+							selected={formValue.backgroundColor}
+							options={backgroundColors}
+							title='Цвет фона'
+							onChange={handleChange('backgroundColor')}
+						/>
+						<Select
+							selected={formValue.containerWidth}
+							options={contentWidthArr}
+							title='Ширина контента'
+							onChange={handleChange('containerWidth')}
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={handleSubmit}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +12,40 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export type StyleValue = {
+	fontFamily: OptionType;
+	fontSize: OptionType;
+	fontColor: OptionType;
+	containerWidth: OptionType;
+	backgroundColor: OptionType;
+};
+
 const App = () => {
+	const [styleValue, setStyleValue] = useState<StyleValue>({
+		fontFamily: defaultArticleState.fontFamilyOption,
+		fontSize: defaultArticleState.fontSizeOption,
+		fontColor: defaultArticleState.fontColor,
+		containerWidth: defaultArticleState.contentWidth,
+		backgroundColor: defaultArticleState.backgroundColor,
+	});
+
+	const handleNewStyles = (newStyles: StyleValue) => {
+		setStyleValue(newStyles);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': styleValue.fontFamily.value,
+					'--font-size': styleValue.fontSize.value,
+					'--font-color': styleValue.fontColor.value,
+					'--container-width': styleValue.containerWidth.value,
+					'--bg-color': styleValue.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSubmit={handleNewStyles} initialState={styleValue} />
 			<Article />
 		</main>
 	);
